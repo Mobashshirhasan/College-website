4b6d4b3bdac00359e4ae238c3f80b247cca4e3ec
Mobashshir Hasan <mobashshir12@gmail.com>
2024-10-28 10:22:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Mobashshir Hasan
+          Learn React with Mobashshir.
         </a>
       </header>
     </div>
